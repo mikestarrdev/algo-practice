429241abe414441eada44fa626b0d4e3ba070cba
Mike Starr <imhs@protonmail.com>
2022-05-27 15:02:09 -0700

    Solved fractorial algo

---
+++ fractorial.js
@@ -0,0 +1,24 @@
+function factorial(num) {
+  if (num === 1) return 1;
+  if (num === 0) return 1;
+
+  let result = num;
+
+  function operation(num) {
+    result *= num - 1;
+    if (num === 2) {
+      return;
+    } else {
+      operation(num - 1);
+    }
+  }
+
+  operation(num);
+  return result;
+}
+
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(4));
+console.log(factorial(7));
+console.log(factorial(17));
